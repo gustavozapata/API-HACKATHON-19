23ab76f38d0dfe5cebf14dd3110e58a1bac19e5f
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-09 00:49:14 -0700

    added landing page + styles + footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import home_img from './components/images/home.png';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="viewport">
+        <header className="App-header">
+          <Header />
+        </header>
+        <main>
+          <p>Connect to your bank, get finance recommendations and learn about the latest trends in the market</p>
+          <img src={home_img} /><br />
+          <button>Get Started</button>
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='Footer'>
+            <a href='https://github.com/gustavozapata/api-hackathon-19' target='_blank'>GitHub</a>
+            <p>Designed and Developed by (Team name)</p>
+        </div>
+    )
+}
