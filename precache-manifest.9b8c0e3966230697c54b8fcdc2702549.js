a98f8e84df8240229f49078bc2e98c8aec437a2c
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-10 10:06:27 -0700

    Updates

---
+++ precache-manifest.9b8c0e3966230697c54b8fcdc2702549.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec767a84a50b278f8ca70c8a29fda171",
+    "url": "/api-hackathon-19/index.html"
+  },
+  {
+    "revision": "ba49b47005b0186b45ff",
+    "url": "/api-hackathon-19/static/css/main.996f44bd.chunk.css"
+  },
+  {
+    "revision": "a4160020bfd505e29a38",
+    "url": "/api-hackathon-19/static/js/2.cec5eb69.chunk.js"
+  },
+  {
+    "revision": "ba49b47005b0186b45ff",
+    "url": "/api-hackathon-19/static/js/main.c9841c9a.chunk.js"
+  },
+  {
+    "revision": "3b197c1ca07a97ed1112",
+    "url": "/api-hackathon-19/static/js/runtime-main.d1a5ec0a.js"
+  },
+  {
+    "revision": "74967fcfb771554459ca3e01d662ac18",
+    "url": "/api-hackathon-19/static/media/capital.74967fcf.png"
+  },
+  {
+    "revision": "1200262f5591b8fd7ee91c6384b96ef3",
+    "url": "/api-hackathon-19/static/media/chase.1200262f.png"
+  },
+  {
+    "revision": "8a028e8d8f3233dcaa103f0c41458724",
+    "url": "/api-hackathon-19/static/media/fargo.8a028e8d.png"
+  },
+  {
+    "revision": "3fcb2f7c273aea9219ea88b203c7a0d5",
+    "url": "/api-hackathon-19/static/media/home.3fcb2f7c.png"
+  }
+]);
