fec81db477a80e5e1e13b03d5985c70c3d471026
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-10 10:58:39 -0700

    Updates

--- backend/proxy/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from proxy import views
-
-urlpatterns = [
-    path('get_access_token/', views.get_access_token)
-]
---
+++ precache-manifest.028845c460b2647991802a85093c8aab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3762e01ac54023510a70a87074ce439",
+    "url": "/api-hackathon-19/index.html"
+  },
+  {
+    "revision": "2e11edbdf9b43f2f8551",
+    "url": "/api-hackathon-19/static/css/main.996f44bd.chunk.css"
+  },
+  {
+    "revision": "a4160020bfd505e29a38",
+    "url": "/api-hackathon-19/static/js/2.cec5eb69.chunk.js"
+  },
+  {
+    "revision": "2e11edbdf9b43f2f8551",
+    "url": "/api-hackathon-19/static/js/main.41225d01.chunk.js"
+  },
+  {
+    "revision": "3b197c1ca07a97ed1112",
+    "url": "/api-hackathon-19/static/js/runtime-main.d1a5ec0a.js"
+  },
+  {
+    "revision": "74967fcfb771554459ca3e01d662ac18",
+    "url": "/api-hackathon-19/static/media/capital.74967fcf.png"
+  },
+  {
+    "revision": "1200262f5591b8fd7ee91c6384b96ef3",
+    "url": "/api-hackathon-19/static/media/chase.1200262f.png"
+  },
+  {
+    "revision": "8a028e8d8f3233dcaa103f0c41458724",
+    "url": "/api-hackathon-19/static/media/fargo.8a028e8d.png"
+  },
+  {
+    "revision": "3fcb2f7c273aea9219ea88b203c7a0d5",
+    "url": "/api-hackathon-19/static/media/home.3fcb2f7c.png"
+  }
+]);
