2154fba7204829bd8a1774ef4291d76ea56e32e3
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-10-10 11:26:13 -0700

    restoring files

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.css';
+
+import Home from './components/Home';
+import ConnectBank from './components/ConnectBank';
+import LoginBank from './components/LoginBank';
+import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div id="viewport">
+          <header className="App-header">
+            <Header />
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/connect-bank" component={ConnectBank} />
+              <Route path="/login-bank" component={LoginBank} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+import userData from '../userData';
+
+export class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userFinance: [],
+            dataUser: userData,
+            topExpenses: [],
+            user: 'gustavozapata',
+            score: 75
+        };
+    }
+
+    componentDidMount() {
+        this.fetchUserData();
+        this.getUserData();
+    }
+
+    getUserData() {
+        const entries = Object.entries(this.state.dataUser);
+
+        let sortEntries = entries.sort((a, b) => a[1] - b[1]);
+        sortEntries.reverse();
+
+        const topCategories = []
+        let total = 0
+
+        for (let i = 2; i < 7; i++) {
+            topCategories[i - 2] = {
+                id: i - 1,
+                category: sortEntries[i][0],
+                value: +(sortEntries[i][1]).toFixed(2)
+            }
+            total += +(sortEntries[i][1]).toFixed(2)
+        }
+
+        this.setState({
+            topExpenses: [...topCategories],
+            total: total
+        })
+    }
+
+    fetchUserData = () => {
+        const gzApi = `https://gustavozapata.me/api/get-vendors.php`
+
+        axios.get(gzApi)
+            .then((res) => {
+                this.setState({
+                    userFinance: [...res.data]
+                })
+            }, (error) => {
+                console.error('An error occurred when making the request...')
+            })
+    };
+
+    render() {
+        return (
+            <div>
+                <Link to='/' style={{ color: 'white', float: 'right', fontSize: '.9em' }}>
+                    Log out
+                </Link>
+                <p style={{ fontWeight: 'bold' }}>Welcome <span style={{ color: '#18C4ED' }}>{this.state.user}</span></p>
+                <div className="score">
+                    <h2>Your Score</h2>
+                    <div className="elScore">
+                        <span id='circle'><a>{this.state.score}<span>/100</span></a></span>
+                    </div>
+                </div>
+                <div className="bar-container">
+                    <div className="elBar">
+                        {this.state.topExpenses.map(item => (  //TODO: HARDCODED JSON DATA
+                            <div className="lasBars">
+                                <p>&#36; {item.value}</p>
+                                <span className={`bar bar-${item.id}`} style={{ width: `${((item.value / this.state.total) * 100) + 60}%` }}>{item.category}</span>
+                            </div>
+                        ))}
+                    </div>
+                    <h2>Total Expenses: &#36;{this.state.total}</h2>
+                </div>
+
+                {/* {this.state.userFinance.map(item => (  //TODO: FETCH DATA
+                    <a>{item.browser}</a>
+                ))} */}
+            </div>
+        )
+    }
+}
+
+export default Dashboard
